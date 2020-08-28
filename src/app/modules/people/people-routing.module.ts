d9a2774a5bd71642225c1657b8515efd802c5b57
Freddy Silber <freddy.silber@cloudcoach.com>
2020-08-28 11:32:59 -0600

    clean up

--- src/app/modules/people/people-routing.module.ts
+++ src/app/modules/people/people-routing.module.ts
@@ -2,17 +2,12 @@ import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 
 import { PeopleComponent } from './people.component'
-// import { PersonDetailComponent } from './person-detail/person-detail.component'
 
 const routes: Routes = [
 	{
 		path: '',
 		component: PeopleComponent
 	}
-	// {
-	// 	path: ':id',
-	// 	component: PersonDetailComponent
-	// }
 ]
 
 @NgModule({
